57e440c539c3ac3b7891fec869a5f4ad71f643bb
AMS777 <alberto@oscuro.es>
2021-02-26 00:11:40 +0100

    Join tests in one integral file

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -11,4 +11,14 @@ describe('Homepage', () => {
     cy.get(ids.homepage.title).should('exist');
     cy.get(ids.homepage.description).should('exist');
   });
+
+  it('has search field and button', () => {
+    cy.get(ids.search.field).should('exist');
+    cy.get(ids.search.button).should('exist');
+  });
+
+  it('has package title and tree view', () => {
+    cy.get(ids.packageDependencies.title).should('exist');
+    cy.get(ids.packageDependencies.treeView).should('exist');
+  });
 });
