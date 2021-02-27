e12b63b6c8d931c2abe5b997b9b6d7dd959a459e
AMS777 <alberto@oscuro.es>
2021-02-27 16:08:33 +0100

    Show dependencies tree

--- src/components/PackageDependencies.jsx
+++ src/components/PackageDependencies.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import { PackageContext } from '../context';
@@ -8,12 +8,56 @@ import Typography from '@material-ui/core/Typography';
 
 function PackageDependencies() {
   const packageContext = useContext(PackageContext);
+  const [dependencies, setDependencies] = useState('');
+
+  const getPackage = async name => await fetch(`/${name}/latest`).then(response => response.json());
 
   const { data, isLoading } = useQuery({
     queryKey: packageContext.name,
-    queryFn: () => fetch(`/${packageContext.name}/latest`).then(response => response.json()),
+    queryFn: () => getPackage(packageContext.name),
     enabled: !!packageContext.name,
   });
+
+  useEffect(() => {
+    const getSubdependencies = async npmPackage => {
+      if (!npmPackage.dependencies) {
+        return;
+      }
+
+      const subdependencies = await Promise.all(
+        Object.entries(npmPackage.dependencies).map(async dependency => {
+          const dependencies = await getDependencies(dependency[0]);
+          return dependencies;
+        }),
+      );
+
+      return subdependencies;
+    };
+
+    const getDependencies = async name => {
+      if (!name) {
+        return;
+      }
+
+      const npmPackage = await getPackage(name);
+
+      const subdependencies = await getSubdependencies(npmPackage);
+
+      return (
+        <li key={name}>
+          {name}
+          <ol>{subdependencies}</ol>
+        </li>
+      );
+    };
+
+    const fetchDependencies = async () => {
+      const d = await getDependencies(packageContext.name);
+      setDependencies(<ol className="parent-npm-package">{d}</ol>);
+    };
+
+    fetchDependencies();
+  }, [packageContext.name]);
 
   const sectionTitle = !packageContext.name
     ? '- Search a package to list its dependencies -'
@@ -30,17 +74,7 @@ function PackageDependencies() {
       </Typography>
       {validPackage && (
         <div data-test="package-dependencies-tree-view">
-          {isLoading ? (
-            'Loading...'
-          ) : !data?.dependencies ? (
-            '- No dependencies -'
-          ) : (
-            <ul>
-              {Object.entries(data.dependencies).map(([packageName, version]) => (
-                <li key={packageName}>{`${packageName}: ${version}`}</li>
-              ))}
-            </ul>
-          )}
+          {isLoading ? 'Loading...' : !dependencies ? '- No dependencies -' : dependencies}
         </div>
       )}
     </Paper>
