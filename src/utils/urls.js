16df5001d9be4e620e72e751261e1f85265f893b
AMS777 <alberto@oscuro.es>
2021-02-28 03:24:12 +0100

    Publish to Github Pages

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,4 @@
 export function makeNpmPackageUrl(packageName) {
-  return `/${packageName}/latest`;
+  // return `/${packageName}/latest`;
+  return `https://registry.npmjs.org/${packageName}/latest`;
 }
